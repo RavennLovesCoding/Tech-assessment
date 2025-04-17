9127e1191939bbc04d53075a94c1bddb7333b8aa
Ravenn <ravennmb@gmail.com>
2025-04-16 21:07:46 -0600

    read more button is now functional and shows remaining book data

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../src/App.css';
 import books from "../src/books.json";
 
 function App () {
+  const [readMore, setReadMore] = useState({});
+  
+  const toggleReadMore  = (index) => {
+    setReadMore((prev) => ({
+      ...prev,
+      [index]: !prev[index]
+    }))
+  };
+
   return (
     <div>
       <h1>Books</h1>
-      
-      <ul>
-        <div className="bookCard">
+      <div>
+      <ul className="bookCard">
         {books.map((book, index) => (
           <li key={index} style={{ marginBottom: '20px' }}>
             <p>Author: {book.author}</p>
@@ -19,11 +27,29 @@ function App () {
               alt={`Cover of ${book.title}`} 
               style={{ width: '150px', height: 'auto', display: 'block', marginBottom: '10px' }}
             />
-            <p>Read More <button>+</button></p>
-          </li>
+            <p>Read More <button onClick={() => toggleReadMore(index)}>+</button></p>
+
+            {readMore[index] && (
+                <div>
+                  <p>Country: {book.country}</p>
+                  <p>Language: {book.language}</p>
+                  <p>
+                    More Info:{' '}
+                    <a href={book.link} target="_blank" rel="noopener noreferrer">
+                      Wikipedia
+                    </a>
+                  </p>
+                  <p>Year Published:{book.year}</p>
+                </div>
+              )}
+            </li>
+
+
+          
         ))}
-        </div>
+        
       </ul>
+      </div>
     
     </div>
   );
